36cc5c272021a6ab9a13ba5f566792cfe27ff563
Dvis <gokhaledurgesh4@gmail.com>
2024-11-24 21:54:32 +0530

    Auto Fixes

--- ERAVIBES/utils/inline/play.py
+++ ERAVIBES/utils/inline/play.py
@@ -60,7 +60,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(text="◁", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
@@ -89,7 +89,7 @@ def stream_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(text="◁", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
