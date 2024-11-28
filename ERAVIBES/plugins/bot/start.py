e16546d38fce08fe453e58ff98087595c055c8f6
Dvis <gokhaledurgesh4@gmail.com>
2024-11-28 21:42:15 +0530

    Auto Fixes

--- ERAVIBES/plugins/bot/start.py
+++ ERAVIBES/plugins/bot/start.py
@@ -26,6 +26,14 @@ from strings import get_string
 IMAGE = [
 "https://files.catbox.moe/ciqfdq.jpg",
 "https://files.catbox.moe/jrfn5c.jpg",
+"https://files.catbox.moe/tb20q7.jpg",
+"https://files.catbox.moe/htuibk.jpg",
+"https://files.catbox.moe/rpghqf.jpg",
+"https://files.catbox.moe/70p4kt.jpg",
+"https://files.catbox.moe/cbr208.jpg",
+"https://files.catbox.moe/3fhcv2.jpg",
+"https://files.catbox.moe/3m0pth.jpg",
+"https://files.catbox.moe/pbekqf.jpg",
 ]
 
 # Updated list with emojis
