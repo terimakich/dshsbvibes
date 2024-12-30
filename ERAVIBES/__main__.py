ca518463eff06ae5b6f77c61ea154f5aa3202a7c
Dvis <gokhaledurgesh4@gmail.com>
2024-12-30 22:50:43 +0530

    Update __main__.py

--- ERAVIBES/__main__.py
+++ ERAVIBES/__main__.py
@@ -2,15 +2,13 @@ import asyncio
 import importlib
 
 from pyrogram import idle
-from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from ERAVIBES import LOGGER, app, userbot
+from config import BANNED_USERS
+from ERAVIBES import HELPABLE, LOGGER, app, userbot
 from ERAVIBES.core.call import ERA
-from ERAVIBES.misc import sudo
 from ERAVIBES.plugins import ALL_MODULES
 from ERAVIBES.utils.database import get_banned_users, get_gbanned
-from config import BANNED_USERS
 
 
 async def init():
@@ -21,9 +19,15 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error("✦ Assistant client variables not defined, exiting...")
-        exit()
-    await sudo()
+        LOGGER("ERAVIBES").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
+        LOGGER("ERAVIBES").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -31,32 +35,26 @@ async def init():
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
-    except:
+    except Exception:
         pass
+
     await app.start()
+
     for all_module in ALL_MODULES:
-        importlib.import_module("ERAVIBES.plugins" + all_module)
-    LOGGER("ERAVIBES.plugins").info("✦ Successfully Imported Modules...💞")
+        imported_module = importlib.import_module(all_module)
+
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+    LOGGER("ERAVIBES.plugins").info("𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 𝗜𝗺𝗽𝗼𝗿𝘁𝗲𝗱 𝗔𝗹𝗹 𝗠𝗼𝗱𝘂𝗹𝗲𝘀 ✅")
+
     await userbot.start()
     await ERA.start()
-    try:
-        await ERA.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
-    except NoActiveGroupCall:
-        LOGGER("ERAVIBES").error(
-            "✦ Please turn on the videochat of your log group\channel.\n\n✦ Stopping Bot...💣"
-        )
-        exit()
-    except:
-        pass
     await ERA.decorators()
-    LOGGER("ERAVIBES").info(
-        "✦ Created By ➥ The Dvis...🐝"
-    )
+    LOGGER("ERAVIBES").info("🎉 𝗘𝗥𝗔𝗩𝗜𝗕𝗘𝗦🥳𝗦𝗧𝗔𝗥𝗧𝗘𝗗🥳𝗦𝗨𝗖𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟𝗬 🎊")
     await idle()
-    await app.stop()
-    await userbot.stop()
-    LOGGER("ERAVIBES").info("❖ Stopping ERA Music Bot...💌")
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init())
+    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
+    LOGGER("ERAVIBES").info("𝗦𝘁𝗼𝗽𝗽𝗶𝗻𝗴 𝗘𝗥𝗔𝗩𝗜𝗕𝗘𝗦! 𝗚𝗼𝗼𝗱𝗕𝘆𝗲 🥹")
