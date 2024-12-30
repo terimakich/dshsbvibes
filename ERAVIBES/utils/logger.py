42c2bf0fe3cce80ef26e0860c38b2e673087928f
Dvis <gokhaledurgesh4@gmail.com>
2024-12-30 14:29:44 +0530

    v1.1

--- ERAVIBES/utils/logger.py
+++ ERAVIBES/utils/logger.py
@@ -8,18 +8,16 @@ from config import LOGGER_ID
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
- <b>❖ {app.mention} ᴘʟᴀʏ ʟᴏɢ ❖</b>
-
-<b>● ᴄʜᴀᴛ ɪᴅ ➥</b> <code>{message.chat.id}</code>
+ <blockquote><b>❖ {app.mention} ᴘʟᴀʏ ʟᴏɢ ❖</b></blockquote>
+<blockquote><b>● ᴄʜᴀᴛ ɪᴅ ➥</b> <code>{message.chat.id}</code>
 <b>● ᴄʜᴀᴛ ɴᴀᴍᴇ ➥</b> {message.chat.title}
-<b>● ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ ➥</b> @{message.chat.username}
-
-<b>● ᴜsᴇʀ ɪᴅ ➥</b> <code>{message.from_user.id}</code>
+<b>● ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ ➥</b> @{message.chat.username}</blockquote>
+<blockquote><b>● ᴜsᴇʀ ɪᴅ ➥</b> <code>{message.from_user.id}</code>
 <b>● ɴᴀᴍᴇ ➥</b> {message.from_user.mention}
-<b>● ᴜsᴇʀɴᴀᴍᴇ ➥</b> @{message.from_user.username}
-
-<b>● ǫᴜᴇʀʏ ➥</b> {message.text.split(None, 1)[1]}
-<b>● sᴛʀᴇᴀᴍᴛʏᴘᴇ ➥</b> {streamtype}"""
+<b>● ᴜsᴇʀɴᴀᴍᴇ ➥</b> @{message.from_user.username}</blockquote>
+</blockquote><b>● ǫᴜᴇʀʏ ➥</b> {message.text.split(None, 1)[1]}
+<b>● ᴅᴜʀᴀᴛɪᴏɴ ➥ </b> {duration_min}
+<b>● sᴛʀᴇᴀᴍᴛʏᴘᴇ ➥</b> {streamtype}</blockquote>"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
