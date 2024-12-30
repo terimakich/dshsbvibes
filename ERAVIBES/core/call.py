807dd67d3b6968a9f0ffe4f3d68b455ad37c9ce0
Dvis <gokhaledurgesh4@gmail.com>
2024-12-30 23:30:24 +0530

    Update call.py

--- ERAVIBES/core/call.py
+++ ERAVIBES/core/call.py
@@ -12,6 +12,7 @@ from pytgcalls.exceptions import (
     TelegramServerError,
 )
 from pytgcalls.types import Update
+from pyrogram.enums import ChatMembersFilter, ChatMemberStatus
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
 from pytgcalls.types.stream import StreamAudioEnded
@@ -47,6 +48,51 @@ async def _clear_(chat_id):
     await remove_active_video_chat(chat_id)
     await remove_active_chat(chat_id)
 
+async def _clear_(chat_id):
+    # Clearing the chat ID data in the database
+    db[chat_id] = []
+
+    # Removing active video chat and chat records
+    try:
+        await remove_active_video_chat(chat_id)
+        await remove_active_chat(chat_id)
+    except Exception as e:
+        print(f"Error removing active chats: {e}")
+
+    # Preparing the text for admin mentions
+    text = ""
+
+    try:
+        # Fetching admins from the chat
+        admins = [
+            admin.user.id
+            async for admin in app.get_chat_members(
+                chat_id, filter=ChatMembersFilter.ADMINISTRATORS
+            )
+        ]
+
+        # Looping through each admin to check if they are not a bot or deleted
+        for admin in admins:
+            admin_member = await app.get_chat_member(chat_id, admin)
+            if not admin_member.user.is_bot and not admin_member.user.is_deleted:
+                text += f"[\u2063](tg://user?id={admin})"
+    except Exception as e:
+        await app.send_message(
+            chat_id,
+            f"**ᴄᴏᴜʟᴅ ɪ ɢᴇᴛ ᴀᴅᴍɪɴ ᴀᴄᴄᴇss? ɪᴛ ᴡɪʟʟ ʜᴇʟᴘ ᴋᴇᴇᴘ ᴛʜᴇ sᴏɴɢs ᴘʟᴀʏɪɴɢ ᴍᴏʀᴇ ʀᴇʟɪᴀʙʟʏ. ᴛʜᴀɴᴋs ɪɴ ᴀᴅᴠᴀɴᴄᴇ! 🎵😊{text}**",
+        )
+
+    # Sending the final message
+    try:
+        AMBOT = await app.send_message(
+                chat_id,
+                f"**🎧 ꜱᴏɴɢ ʜᴀꜱ ᴇɴᴅᴇᴅ ɪɴ ᴠᴄ🥺**{text}",
+        )
+        await asyncio.sleep(10)
+        await AMBOT.delete()
+        
+    except Exception as e:
+        print(f"Error sending message: {e}")
 
 class Call(PyTgCalls):
     def __init__(self):
