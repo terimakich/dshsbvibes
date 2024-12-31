317080bde32170f37833f14d45bb663c69c7d037
Dvis <gokhaledurgesh4@gmail.com>
2024-12-31 13:54:09 +0530

    Update start.py

--- ERAVIBES/plugins/bot/start.py
+++ ERAVIBES/plugins/bot/start.py
@@ -123,7 +123,9 @@ async def testbot(client, message: Message, _):
     except Exception as e:
         pass
         
-    await message.reply_text(_["start_1"])
+    #await message.reply_text(_["start_1"])
+     await message.reply_text(_["start_1"].format(config.OWNER_ID),
+                reply_markup=keyboard)
 
     return await add_served_chat(message.chat.id)
 
