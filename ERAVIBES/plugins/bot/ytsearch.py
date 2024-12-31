ea44f6602888b7b19280524493b14b1a13581fd1
Dvis <gokhaledurgesh4@gmail.com>
2024-12-31 22:29:02 +0530

    Update ytsearch.py

--- ERAVIBES/plugins/bot/ytsearch.py
+++ ERAVIBES/plugins/bot/ytsearch.py
@@ -18,12 +18,12 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 5:
-            text += f"<blockquote>❖ ᴠɪᴅᴇᴏ ɴᴀᴍᴇ ➥ {results[i]['title']}</blockquote>"
+            text += f"<blockquote>❖ ᴠɪᴅᴇᴏ ɴᴀᴍᴇ ➥ {results[i]['title']}</blockquote>\n"
             text += "<blockquote>"
             text += f"● ᴠɪᴅᴇᴏ ᴅᴜʀᴀᴛɪᴏɴ ➥ {results[i]['duration']}\n"
             text += f"● ᴠɪᴅᴇᴏ ᴠɪᴇᴡs ➥ {results[i]['views']}\n"
             text += f"● ᴠɪᴅᴇᴏ ᴄʜᴀɴɴᴇʟ ➥ {results[i]['channel']}\n"
-            text += f"● ᴠɪᴅᴇᴏ ᴜʀʟ ➥ https://www.youtube.com{results[i]['url_suffix']}\n"
+            text += f"● ᴠɪᴅᴇᴏ ᴜʀʟ ➥ https://www.youtube.com{results[i]['url_suffix']}\n\n"
             text += "</blockquote>"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
