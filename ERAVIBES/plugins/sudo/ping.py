87116a7272c3f03af3d4f79a6f793a17447b0f50
Dvis <gokhaledurgesh4@gmail.com>
2025-01-01 14:04:39 +0530

    Create ping.py

---
+++ ERAVIBES/plugins/sudo/ping.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from ERAVIBES import app
+from ERAVIBES.core.call import ERA 
+from ERAVIBES.utils import bot_sys_stats
+from ERAVIBES.utils.inline import supp_markup
+from config import BANNED_USERS, D
+from strings import strings
+
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+async def ping(client, message: Message, _):
+    try:
+        # React with random emoji
+        await message.react(random.choice(D))
+    except Exception as e:
+        print(f"Reaction Error: {e}")
+
+    start = datetime.now()
+    response = await message.reply_photo(
+        caption=strings["ping_1"].format(app.mention),
+    )
+    pytgping = await ERA.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        strings["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(),
+    )
