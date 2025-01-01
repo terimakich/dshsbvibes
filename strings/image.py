62222fbfed505c8fe98e6935b01f3363ca04f5fe
Dvis <gokhaledurgesh4@gmail.com>
2025-01-01 14:53:17 +0530

    Create image.py

---
+++ strings/image.py
@@ -0,0 +1,12 @@
+IMAGE = [
+"https://files.catbox.moe/ciqfdq.jpg",
+"https://files.catbox.moe/jrfn5c.jpg",
+"https://files.catbox.moe/tb20q7.jpg",
+"https://files.catbox.moe/htuibk.jpg",
+"https://files.catbox.moe/rpghqf.jpg",
+"https://files.catbox.moe/70p4kt.jpg",
+"https://files.catbox.moe/cbr208.jpg",
+"https://files.catbox.moe/3fhcv2.jpg",
+"https://files.catbox.moe/3m0pth.jpg",
+"https://files.catbox.moe/pbekqf.jpg",
+]
