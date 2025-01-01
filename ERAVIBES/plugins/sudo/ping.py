dcd5a2d72e9471971fe0b8a8ceb3709b7d3a29d1
Dvis <gokhaledurgesh4@gmail.com>
2025-01-01 15:07:15 +0530

    Update ping.py

--- ERAVIBES/plugins/sudo/ping.py
+++ ERAVIBES/plugins/sudo/ping.py
@@ -7,12 +7,16 @@ from ERAVIBES import app
 from ERAVIBES.core.call import ERA 
 from ERAVIBES.utils import bot_sys_stats
 from ERAVIBES.utils.inline import supp_markup
+from ERAVIBES.utils.decorators.language import language
 from config import BANNED_USERS, D
 from strings import get_string
 
 
+from ERAVIBES.utils.decorators.language import language
+
 @app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
-async def ping(client, message: Message):
+@language  # This decorator provides the 'lang' argument
+async def ping(client, message: Message, lang):
     try:
         # React with random emoji
         await message.react(random.choice(D))
@@ -21,12 +25,12 @@ async def ping(client, message: Message):
 
     start = datetime.now()
     response = await message.reply_photo(
-        caption=get_string()["ping_1"].format(app.mention),  # Fixed here
+        caption=get_string(lang)["ping_1"].format(app.mention),  # Provide 'lang' argument
     )
     pytgping = await ERA.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
-        get_string()["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),  # Fixed here
+        get_string(lang)["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),  # Provide 'lang' argument
         reply_markup=supp_markup(),
     )
