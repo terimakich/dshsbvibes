d53d01cb854b755d1ccc2462495e8260cfb19eaa
Dvis <gokhaledurgesh4@gmail.com>
2025-01-01 15:38:21 +0530

    Update ping.py

--- ERAVIBES/plugins/sudo/ping.py
+++ ERAVIBES/plugins/sudo/ping.py
@@ -12,23 +12,23 @@ from ERAVIBES.utils.database import get_lang
 from config import BANNED_USERS, D
 from strings import get_string
 
-
 @app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
 @language
 async def ping(client, message: Message, lang):
     try:
+        lang_code = lang.get("language_code", "en")  # Extract language code
         await message.react(random.choice(D))
     except Exception as e:
         print(f"Reaction Error: {e}")
 
     start = datetime.now()
     response = await message.reply_text(
-        text=get_string(get_lang)["ping_1"].format(app.mention),
+        text=get_string(lang_code)["ping_1"].format(app.mention),
     )
     pytgping = await ERA.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
-        text=get_string(get_lang)["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        text=get_string(lang_code)["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
         reply_markup=supp_markup(),
     )
