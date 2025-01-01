13b88d1c7e982e4f7de5f06fb0c58ba2ec22d93a
Dvis <gokhaledurgesh4@gmail.com>
2025-01-01 16:00:41 +0530

    Update ping.py

--- ERAVIBES/plugins/sudo/ping.py
+++ ERAVIBES/plugins/sudo/ping.py
@@ -3,35 +3,33 @@ from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message
 
+from config import BANNED_USERS
 from ERAVIBES import app
-from ERAVIBES.core.call import ERA 
+from ERAVIBES.core.call import ERA
 from ERAVIBES.utils import bot_sys_stats
-from ERAVIBES.utils.inline import supp_markup
 from ERAVIBES.utils.decorators.language import language
-from ERAVIBES.utils.database import get_lang
-from config import BANNED_USERS, D
-from strings import get_string
+from ERAVIBES.utils.inline import support_group_markup
 
-from ERAVIBES.utils.decorators.language import language
 
 @app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
 @language
-async def ping(client, message: Message, lang):
-    try:
-        # React with a random emoji
-        await message.react(random.choice(D))
-    except Exception as e:
-        print(f"Reaction Error: {e}")
-
-    start = datetime.now()
+async def ping_com(client, message: Message, _):
     response = await message.reply_text(
-        text=get_string(lang)["ping_1"].format(app.mention)
+        text=_["ping_1"].format(app.mention),
     )
+    start = datetime.now()
     pytgping = await ERA.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
-
     await response.edit_text(
-        text=get_string(lang)["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(lang),  # Pass 'lang' to supp_markup
+        _["ping_2"].format(
+            resp,
+            app.mention,
+            UP,
+            RAM,
+            CPU,
+            DISK,
+            pytgping,
+        ),
+        reply_markup=support_group_markup(_),
     )
