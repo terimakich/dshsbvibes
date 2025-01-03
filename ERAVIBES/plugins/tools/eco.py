fb779af727054552e5c2e8911e2b25fe1ff940e0
Dvis <gokhaledurgesh4@gmail.com>
2025-01-03 22:02:52 +0530

    Update eco.py

--- ERAVIBES/plugins/tools/eco.py
+++ ERAVIBES/plugins/tools/eco.py
@@ -1,24 +1,26 @@
 from pyrogram import Client, filters
-#from ERAVIBES.misc import SUDOERS
 from config import OWNER_ID
 from ERAVIBES import app 
 
 # /eco command handler
-app.on_message(filters.command("eco") & filters.reply & OWNER_ID)
+@app.on_message(filters.command("eco") & filters.reply & filters.user(OWNER_ID))
 async def eco_reply(client, message):
-    if not message.reply_to_message:
-        await message.reply("Please reply to a user's message to use this command.")
-        return
+    try:
+        if not message.reply_to_message:
+            await message.reply("Please reply to a user's message to use this command.")
+            return
 
-    # Split the command from the message (removing the command /eco)
-    command_text = message.text.split(" ", 1)
+        # Split the command from the message (removing the command /eco)
+        command_text = message.text.split(" ", 1)
 
-    if len(command_text) < 2:
-        await message.reply("Please provide a message after the /eco command.")
-        return
+        if len(command_text) < 2:
+            await message.reply("Please provide a message after the /eco command.")
+            return
 
-    # The message to reply with
-    reply_message = command_text[1]
+        # The message to reply with
+        reply_message = command_text[1]
 
-    await message.delete()
-    await message.reply_to_message.reply(reply_message)
+        await message.delete()
+        await message.reply_to_message.reply(reply_message)
+    except Exception as e:
+        await message.reply(f"An error occurred: {e}")
