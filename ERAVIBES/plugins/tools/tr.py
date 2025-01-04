c322274a9b9f361bbe090cf7241fb74ef8a4befe
Dvis <gokhaledurgesh4@gmail.com>
2025-01-04 22:03:54 +0530

    Update tr.py

--- ERAVIBES/plugins/tools/tr.py
+++ ERAVIBES/plugins/tools/tr.py
@@ -9,13 +9,13 @@ async def translate(client, message):
         target_language = message.text.split(None, 1)[1] if len(message.text.split()) > 1 else 'en'
     else:
         if len(message.text.split()) < 3:
-            await message.reply_text("You can use this command by replying to a message.")
+            await message.reply_text("❖ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ʙʏ ʀᴇᴘʟʏɪɴɢ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ...")
             return
         target_language = message.text.split(None, 2)[1]
         text_to_translate = message.text.split(None, 2)[2]
 
     try:
         translated = GoogleTranslator(source='auto', target=target_language).translate(text_to_translate)
-        await message.reply_text(f"<b>ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ</b> {target_language}\n\n {translated}")
+        await message.reply_text(f"<b>ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ</b> {target_language}\n\n<code>{translated}</code>")
     except Exception as e:
         await message.reply_text(f"An error occurred during translation: {str(e)}")
