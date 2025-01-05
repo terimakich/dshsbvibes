b70227186725cb9c82216fdf2b7b0e836b4d1424
Dvis <gokhaledurgesh4@gmail.com>
2025-01-05 11:51:08 +0530

    Update autoend.py

--- ERAVIBES/plugins/sudo/autoend.py
+++ ERAVIBES/plugins/sudo/autoend.py
@@ -8,16 +8,16 @@ from ERAVIBES.utils.database import autoend_off, autoend_on
 
 @app.on_message(filters.command("autoend") & SUDOERS)
 async def auto_end_stream(_, message: Message):
-    usage = "❖ <b>ᴇxᴀᴍᴘʟᴇ ➥</b> /autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
+    usage = "❖ <b>ᴇxᴀᴍᴘʟᴇ ➥</b> /autoend [on | off]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
-    if state == "enable":
+    if state == "on":
         await autoend_on()
         await message.reply_text(
             "❖ ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴇɴᴀʙʟᴇᴅ.\n\n● ᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
         )
-    elif state == "disable":
+    elif state == "off":
         await autoend_off()
         await message.reply_text("❖ ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
     else:
