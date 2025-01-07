4bf168701e8eb7e3dfe5d2afe92c6f6469f4be0d
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2025-01-07 10:28:29 +0530

    Update stream.py

--- ERAVIBES/utils/stream/stream.py
+++ ERAVIBES/utils/stream/stream.py
@@ -11,7 +11,7 @@ from ERAVIBES.misc import db
 from ERAVIBES.utils.database import add_active_video_chat, is_active_chat
 from ERAVIBES.utils.exceptions import AssistantErr
 from ERAVIBES.utils.inline import aq_markup, close_markup, stream_markup
-from ERAVIBES.utils.pastebin import Bin
+from ERAVIBES.utils.pastebin import ERABin
 from ERAVIBES.utils.stream.queue import put_queue, put_queue_index
 from ERAVIBES.utils.thumbnails import get_thumb
 
@@ -78,18 +78,7 @@ async def stream(
                         vidid, mystic, video=status, videoid=True
                     )
                 except:
-                    await mystic.delete()
-                    await app.send_message(
-                        LOG_GROUP_ID,
-                        f"<b>ʜᴇʏ <a href='tg://user?id={OWNER_ID[0]}'>ᴏᴡɴᴇʀ</a> ᴍᴀʏ ʙᴇ ᴍʏ ᴄᴏᴏᴋɪᴇs ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴅ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴏɴᴇ ᴛɪᴍᴇ ʙʏ ᴘʟᴀʏ ᴀɴʏ sᴏɴɢs</b>",
-                        parse_mode="HTML"
-                    )
-                    
-                    await app.send_message(
-                        OWNER_ID[0],
-                        f"<b>ʜᴇʏ <a href='tg://user?id={OWNER_ID[0]}'>ᴏᴡɴᴇʀ</a> ᴍᴀʏ ʙᴇ ᴍʏ ᴄᴏᴏᴋɪᴇs ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴅ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴏɴᴇ ᴛɪᴍᴇ ʙʏ ᴘʟᴀʏ ᴀɴʏ sᴏɴɢs</b>",
-                        parse_mode="HTML"
-                    )
+                    raise AssistantErr(_["play_14"])
                 await ERA.join_call(
                     chat_id,
                     original_chat_id,
@@ -110,7 +99,7 @@ async def stream(
                     forceplay=forceplay,
                 )
                 img = await get_thumb(vidid)
-                button = stream_markup(_, vidid, chat_id)
+                button = stream_markup(_, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
                     photo=img,
@@ -127,7 +116,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await Bin(msg)
+            link = await ERABin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -153,19 +142,7 @@ async def stream(
                 vidid, mystic, videoid=True, video=status
             )
         except:
-            await mystic.delete()
-            await app.send_message(
-                LOG_GROUP_ID,
-                f"<b>ʜᴇʏ <a href='tg://user?id={OWNER_ID[0]}'>ᴏᴡɴᴇʀ</a> ᴍᴀʏ ʙᴇ ᴍʏ ᴄᴏᴏᴋɪᴇs ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴅ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴏɴᴇ ᴛɪᴍᴇ ʙʏ ᴘʟᴀʏ ᴀɴʏ sᴏɴɢs</b>",
-                parse_mode="HTML"
-            )
-            
-            await app.send_message(
-                OWNER_ID[0],
-                f"<b>ʜᴇʏ <a href='tg://user?id={OWNER_ID[0]}'>ᴏᴡɴᴇʀ</a> ᴍᴀʏ ʙᴇ ᴍʏ ᴄᴏᴏᴋɪᴇs ʜᴀs ʙᴇᴇɴ ᴅᴇᴀᴅ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ᴏɴᴇ ᴛɪᴍᴇ ʙʏ ᴘʟᴀʏ ᴀɴʏ sᴏɴɢs</b>",
-                parse_mode="HTML"
-            )
-            
+            raise AssistantErr(_["play_14"])
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -208,7 +185,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await get_thumb(vidid)
-            button = stream_markup(_, vidid, chat_id)
+            button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
@@ -261,7 +238,7 @@ async def stream(
                 "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, vidid, chat_id)
+            button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
@@ -315,7 +292,7 @@ async def stream(
             )
             if video:
                 await add_active_video_chat(chat_id)
-            button = stream_markup(_, vidid, chat_id)
+            button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
@@ -376,7 +353,7 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await get_thumb(vidid)
-            button = stream_markup(_, vidid, chat_id)
+            button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
@@ -431,7 +408,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, vidid, chat_id)
+            button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.STREAM_IMG_URL,
