b1490cb8729dfdc008582ec97094922177953349
Dvis <gokhaledurgesh4@gmail.com>
2025-01-07 12:14:19 +0530

    Update stream.py

--- ERAVIBES/utils/stream/stream.py
+++ ERAVIBES/utils/stream/stream.py
@@ -184,6 +184,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
+            chat_id = message.chat.id 
             img = await get_thumb(vidid)
             button = stream_markup(_, chat_id)
             run = await app.send_photo(
