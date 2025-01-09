16e1175beb1c15a77a838f639f3560b94cc6b81f
Dvis <gokhaledurgesh4@gmail.com>
2025-01-09 18:30:34 +0530

    Update chatbot.py

--- ERAVIBES/plugins/tools/chatbot.py
+++ ERAVIBES/plugins/tools/chatbot.py
@@ -5,21 +5,25 @@ from pyrogram.enums import ChatType
 from ERAVIBES import app
 
 @app.on_message(~filters.bot & ~filters.me & filters.text)
-async def chatbot(_:Client,message:Message):
-    if message.chat.type!= ChatType.PRIVATE:
+async def chatbot(_: Client, message: Message):
+    if message.chat.type != ChatType.PRIVATE:
         if not message.reply_to_message:
             return
-        if message.reply_to_message.from_user.id != (await pbot.get_me()).id:
+        if message.reply_to_message.from_user.id != (await app.get_me()).id:
             return
-    if message.text and message.text[0] in ["/", "!", "?", "."]:
-        return
-    response = requests.get("https://sugoi-api.vercel.app/chat?msg="+ message.text)
-    if response.status_code == 200:
-        data = response.json()['response']
-        return await message.reply_text(data)
-    elif response.status_code == 429:
-        return await message.reply_text("ChatBot Error: Too many requests. Please wait a few moments.")
-    elif response.status_code >= 500:
-        return await message.reply_text("ChatBot Error: API server error. Contact us at @CodeSearchDev. ")
-    else:
-        return await message.reply_text("ChatBot Error: Unknown Error Occurred. Contact us at @CodeSearchDev.")
+        if message.text and message.text[0] in ["/", "!", "?", "."]:
+            return
+
+    try:
+        response = requests.get("https://sugoi-api.vercel.app/chat?msg=" + message.text)
+        if response.status_code == 200:
+            data = response.json()['response']
+            await message.reply_text(data)
+        elif response.status_code == 429:
+            await message.reply_text("ChatBot Error: Too many requests. Please wait a few moments.")
+        elif response.status_code >= 500:
+            await message.reply_text("ChatBot Error: API server error. Contact us at @CodeSearchDev.")
+        else:
+            await message.reply_text("ChatBot Error: Unknown Error Occurred. Contact us at @CodeSearchDev.")
+    except requests.exceptions.RequestException as e:
+        await message.reply_text(f"ChatBot Error: Network error occurred. {e}")
