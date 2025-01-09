1f63377370091b67f110a308a4221f763b6174c7
Dvis <gokhaledurgesh4@gmail.com>
2025-01-09 13:15:12 +0530

    Update bot.py

--- ERAVIBES/core/bot.py
+++ ERAVIBES/core/bot.py
@@ -1,5 +1,11 @@
 from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus, ParseMode
+from pyrogram.types import (
+    BotCommand,
+    BotCommandScopeAllChatAdministrators,
+    BotCommandScopeAllGroupChats,
+    BotCommandScopeAllPrivateChats,
+)
 
 import config
 
@@ -43,46 +49,52 @@ class Era(Client):
             exit()
 
                 # Setting commands
-        if config.SET_CMDS:
+        if config.SET_CMDS == str(True):
             try:
                 await self.set_bot_commands(
                     commands=[
-                        BotCommand("start", "Start the bot"),
-                        BotCommand("help", "Get the help menu"),
-                        BotCommand("ping", "Check if the bot is alive or dead"),
+                        BotCommand("start", "❍ 𝗺𝗮𝗸𝗲 𝘁𝗵𝗲 𝗯𝗼𝘁 𝘀𝘁𝗮𝗿𝘁 🚀"),
+                        BotCommand("help", "❍ 𝗴𝗲𝘁 𝘁𝗵𝗲 𝗵𝗲𝗹𝗽 𝗺𝗲𝗻𝘂 ❓"),
+                        BotCommand("ping", "❍ 𝗰𝗵𝗲𝗰𝗸 𝗶𝗳 𝘁𝗵𝗲 𝗯𝗼𝘁 𝗶𝘀 𝗮𝗹𝗶𝘃𝗲 𝗼𝗿 𝗱𝗲𝗮𝗱 💔"),
                     ],
                     scope=BotCommandScopeAllPrivateChats(),
                 )
                 await self.set_bot_commands(
                     commands=[
-                        BotCommand("play", "Start playing requested song"),
-                        BotCommand("stop", "Stop the current song"),
-                        BotCommand("pause", "Pause the current song"),
-                        BotCommand("resume", "Resume the paused song"),
-                        BotCommand("queue", "Check the queue of songs"),
-                        BotCommand("skip", "Skip the current song"),
+                        BotCommand("start", "❍ 𝘀𝘁𝗮𝗿𝘁 𝘁𝗵𝗲 𝗯𝗼𝘁 🚀"),
+                        BotCommand("ping", "❍ 𝗰𝗵𝗲𝗰𝗸 𝘁𝗵𝗲 𝗽𝗶𝗻𝗴 📡"),
+                        BotCommand("help", "❍ 𝗴𝗲𝘁 𝗵𝗲𝗹𝗽 ❓"),
+                        BotCommand("play", "❍ 𝘀𝘁𝗮𝗿𝘁 𝗽𝗹𝗮𝘆𝗶𝗻𝗴 𝗿𝗲𝘃𝘂𝗲𝘀𝘁𝗲𝗱 𝘀𝗼𝗻𝗴 🎵"),
+                        BotCommand("vplay", "❍ 𝗽𝗹𝗮𝘆 𝘃𝗶𝗱𝗲𝗼 𝗮𝗹𝗼𝗻𝗴 𝘄𝗶𝘁𝗵 𝗺𝘂𝘀𝗶𝗰 🎬"),
+                        BotCommand("song", "❍ 𝗱𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝘁𝗵𝗲 𝗿𝗲𝘃𝘂𝗲𝘀𝘁𝗲𝗱 𝘀𝗼𝗻𝗴 🎵"),
+                        BotCommand("yt", "❍ 𝘆𝗼𝘂𝘁𝘂𝗯𝗲 𝘀𝗲𝗮𝗿𝗰𝗵 🔍"),
+                        
                     ],
                     scope=BotCommandScopeAllGroupChats(),
                 )
                 await self.set_bot_commands(
                     commands=[
-                        BotCommand("start", "❥ Start the bot"),
-                        BotCommand("ping", "❥ Check the ping"),
-                        BotCommand("help", "❥ Get help"),
-                        BotCommand("cancel", "❥ Cancel the tagging"),
-                        BotCommand("settings", "❥ Get the settings"),
-                        BotCommand("reload", "❥ Reload the bot"),
-                        BotCommand("play", "❥ Play the requested song"),
-                        BotCommand("vplay", "❥ Play video along with music"),
-                        BotCommand("end", "❥ Empty the queue"),
-                        BotCommand("stop", "❥ Stop the song"),
-                        BotCommand("song", "❥ Download the requested song"),
-                        BotCommand("yt", "❥ Youtube search"),
+                        BotCommand("start", "❍ 𝘀𝘁𝗮𝗿𝘁 𝘁𝗵𝗲 𝗯𝗼𝘁 🚀"),
+                        BotCommand("ping", "❍ 𝗰𝗵𝗲𝗰𝗸 𝘁𝗵𝗲 𝗽𝗶𝗻𝗴 📡"),
+                        BotCommand("help", "❍ 𝗴𝗲𝘁 𝗵𝗲𝗹𝗽 ❓"),
+                        BotCommand("cancel", "❍ 𝗰𝗮𝗻𝗰𝗲𝗹 𝘁𝗵𝗲 𝘁𝗮𝗴𝗴𝗶𝗻𝗴 ❌"),
+                        BotCommand("settings", "❍ 𝗴𝗲𝘁 𝘁𝗵𝗲 𝘀𝗲𝘁𝘁𝗶𝗻𝗴𝘀 ⚙"),
+                        BotCommand("reload", "❍ 𝗿𝗲𝗹𝗼𝗮𝗱 𝘁𝗵𝗲 𝗯𝗼𝘁 🔄"),
+                        BotCommand("play", "❍ 𝗽𝗹𝗮𝘆 𝘁𝗵𝗲 𝗿𝗲𝘃𝘂𝗲𝘀𝘁𝗲𝗱 𝘀𝗼𝗻𝗴 🎵"),
+                        BotCommand("vplay", "❍ 𝗽𝗹𝗮𝘆 𝘃𝗶𝗱𝗲𝗼 𝗮𝗹𝗼𝗻𝗴 𝘄𝗶𝘁𝗵 𝗺𝘂𝘀𝗶𝗰 🎬"),
+                        BotCommand("pause", "❍ 𝗽𝗮𝘂𝘀𝗲 𝘁𝗵𝗲 𝗰𝘂𝗿𝗿𝗲𝗻𝘁 𝘀𝗼𝗻𝗴 ⏸"),
+                        BotCommand("resume", "❍ 𝗿𝗲𝘀𝘂𝗺𝗲 𝘁𝗵𝗲 𝗽𝗮𝘂𝘀𝗲𝗱 𝘀𝗼𝗻𝗴 ▶"),
+                        BotCommand("end", "❍ 𝗲𝗺𝗽𝘁𝘆 𝘁𝗵𝗲 𝘀𝘂𝗲𝘂𝗲 🗑"),
+                        BotCommand("skip", "❍ 𝘀𝗸𝗶𝗽 𝘁𝗵𝗲 𝗰𝘂𝗿𝗿𝗲𝗻𝘁 𝘀𝗼𝗻𝗴 ⏭"),
+                        BotCommand("queue", "❍ 𝗰𝗵𝗲𝗰𝗸 𝘁𝗵𝗲 𝘀𝘂𝗲𝘂𝗲 𝗼𝗳 𝘀𝗼𝗻𝗴𝘀 🗒"),
+                        BotCommand("song", "❍ 𝗱𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝘁𝗵𝗲 𝗿𝗲𝘃𝘂𝗲𝘀𝘁𝗲𝗱 𝘀𝗼𝗻𝗴 🎵"),
+                        BotCommand("yt", "❍ 𝘆𝗼𝘂𝘁𝘂𝗯𝗲 𝘀𝗲𝗮𝗿𝗰𝗵 🔍"),
                     ],
                     scope=BotCommandScopeAllChatAdministrators(),
                 )
             except Exception as e:
-                LOGGER(__name__).error(f"Failed to set bot commands: {e}")
+                LOGGER(__name__).error(f"❖ Failed to set bot commands: {e}")
+
 
         a = await self.get_chat_member(config.LOGGER_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
