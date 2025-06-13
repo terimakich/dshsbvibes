ff9d0ea8c7d3e885ff0d73e30c6d35db2a915d69
Official Durgesh <durgeshgokhale656@gmail.com>
2025-06-13 20:49:56 +0530

    Update __main__.py

--- ERAVIBES/__main__.py
+++ ERAVIBES/__main__.py
@@ -1,29 +1,28 @@
 import asyncio
-import importlib
+import os
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from ERAVIBES import LOGGER, app, userbot
-from ERAVIBES.core.call import ERA
-from ERAVIBES.misc import sudo
-from ERAVIBES.plugins import ALL_MODULES
-from ERAVIBES.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
+from ERAVIBES import HELPABLE, LOGGER, app, userbot
+from ERAVIBES.core.call import Era
+from ERAVIBES.misc import sudo
+from ERAVIBES.utils.database import get_banned_users, get_gbanned
+
+logger = LOGGER("ERAVIBES")
+loop = asyncio.get_event_loop()
 
 
 async def init():
-    if (
-        not config.STRING1
-        and not config.STRING2
-        and not config.STRING3
-        and not config.STRING4
-        and not config.STRING5
-    ):
-        LOGGER(__name__).error("✦ Assistant client variables not defined, exiting...")
-        exit()
-    await sudo()
+    if len(config.STRING_SESSIONS) == 0:
+        logger.error("✦ Assistant client variables not defined, exiting...")
+        return
+    if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
+        logger.warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -31,32 +30,69 @@ async def init():
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
-    except:
+    except Exception:
         pass
+    await sudo()
     await app.start()
-    for all_module in ALL_MODULES:
-        importlib.import_module("ERAVIBES.plugins" + all_module)
-    LOGGER("ERAVIBES.plugins").info("✦ Successfully Imported Modules...💞")
+    for mod in app.load_plugins_from("ERAVIBES/plugins"):
+        if mod and hasattr(mod, "__MODULE__") and mod.__MODULE__:
+            if hasattr(mod, "__HELP__") and mod.__HELP__:
+                HELPABLE[mod.__MODULE__.lower()] = mod
+
+    if config.EXTRA_PLUGINS:
+        if os.path.exists("xtraplugins"):
+            result = await app.run_shell_command(["git", "-C", "xtraplugins", "pull"])
+            if result["returncode"] != 0:
+                logger.error(
+                    f"Error pulling updates for extra plugins: {result['stderr']}"
+                )
+                exit()
+        else:
+            result = await app.run_shell_command(
+                ["git", "clone", config.EXTRA_PLUGINS_REPO, "xtraplugins"]
+            )
+            if result["returncode"] != 0:
+                logger.error(f"Error cloning extra plugins: {result['stderr']}")
+                exit()
+
+        req = os.path.join("xtraplugins", "requirements.txt")
+        if os.path.exists(req):
+            result = await app.run_shell_command(
+                ["uv", "pip", "install", "--system", "-r", req]
+            )
+            if result["returncode"] != 0:
+                logger.error(f"Error installing requirements: {result['stderr']}")
+
+        for mod in app.load_plugins_from("xtraplugins"):
+            if mod and hasattr(mod, "__MODULE__") and mod.__MODULE__:
+                if hasattr(mod, "__HELP__") and mod.__HELP__:
+                    HELPABLE[mod.__MODULE__.lower()] = mod
+
     await userbot.start()
-    await ERA.start()
+    await Era.start()
+    LOGGER("ERAVIBES").info("✦ Successfully Imported Modules...💞")
     try:
-        await ERA.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Era.stream_call(
+            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+        )
     except NoActiveGroupCall:
         LOGGER("ERAVIBES").error(
             "✦ Please turn on the videochat of your log group\channel.\n\n✦ Stopping Bot...💣"
         )
         exit()
-    except:
-        pass
-    await ERA.decorators()
-    LOGGER("ERAVIBES").info(
-        "✦ Created By ➥ The Dvis...🐝"
-    )
+
+    await Era.decorators()
+    LOGGER("ERAVIBES").info("✦ Created By ➥ The Dvis...🐝")
     await idle()
     await app.stop()
     await userbot.stop()
+    await Era.stop()
+
+
+def main():
+    loop.run_until_complete(init())
     LOGGER("ERAVIBES").info("❖ Stopping ERA Music Bot...💌")
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init())
+    main()
